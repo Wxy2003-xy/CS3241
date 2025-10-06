79c702ddf833ab8e2ce043f8bed85a6db2e3849b
Wxy2003-xy <auferstanden1@gmail.com>
2025-10-06 08:46:15 +0800

    no message

--- mt.cpp
+++ mt.cpp
@@ -22,32 +22,157 @@ using namespace std;
 
 #define numStars 100
 #define numPlanets 8
-
-void drawCube2() {
-    
+int mouseButton = 0;
+int moving, startx, starty;
+GLfloat angle = 0;   /* in degrees */
+GLfloat angle2 = 0;   /* in degrees */
+GLfloat zoom = 1.0;
+void drawCube() {
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(0,0,1);
+        glVertex3f(-1,-1, 1);
+        glVertex3f( 1,-1, 1);
+        glVertex3f( 1, 1, 1);
+        glVertex3f(-1, 1, 1);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(0,0,-1);
+        glVertex3f(-1,-1,-1);
+        glVertex3f(-1, 1,-1);
+        glVertex3f( 1, 1,-1);
+        glVertex3f( 1,-1,-1);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(1,0,0);
+        glVertex3f( 1,-1,-1);
+        glVertex3f( 1, 1,-1);
+        glVertex3f( 1, 1, 1);
+        glVertex3f( 1,-1, 1);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(-1,0,0);
+        glVertex3f(-1,-1,-1);
+        glVertex3f(-1,-1, 1);
+        glVertex3f(-1, 1, 1);
+        glVertex3f(-1, 1,-1);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(0,1,0);
+        glVertex3f(-1, 1,-1);
+        glVertex3f(-1, 1, 1);
+        glVertex3f( 1, 1, 1);
+        glVertex3f( 1, 1,-1);
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glNormal3f(0,-1,0);
+        glVertex3f(-1,-1,-1);
+        glVertex3f( 1,-1,-1);
+        glVertex3f( 1,-1, 1);
+        glVertex3f(-1,-1, 1);
+    glEnd();
 }
 
-void drawPyramid() {
+// g++ one_rec_pyramid.cpp -std=c++11 -lGL -lGLU -lglut   (Linux/Windows)
+// g++ one_rec_pyramid.cpp -std=c++11 -framework OpenGL -framework GLUT   (macOS)
 
+#ifdef _WIN32
+  #include <Windows.h>
+  #include "GL/glut.h"
+  #include "GL/glu.h"
+#elif __APPLE__
+  #include <GLUT/glut.h>
+  #include <OpenGL/glu.h>
+#else
+  #include <GL/glut.h>
+  #include <GL/glu.h>
+#endif
+
+// ------- SINGLE function that draws everything and recurses ONCE -------
+void drawPyramid(int n, float y = 0.0f) {
+    glPushMatrix();
+    for (int i = 0; i < n; i++) {
+        glPushMatrix();
+        for (int j = 0; j < n; j++) {
+            glTranslated(0,0,2);
+            drawCube();
+        }
+        glTranslated(2,0,0);
+        glPopMatrix();
+    }
+    glTranslated(1,2,1);
+    if (n>=0) {
+    drawPyramid(n-1);
+    }
+    glPopMatrix();
 }
 
-
-void display() {    
-    glClear(GL_COLOR_BUFFER_BIT);
+// --------- usual GLUT glue (not part of the "one function" constraint) ----------
+void reshape(int w, int h){
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40.0, (double)w/(h?h:1), 0.1, 100.0);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();                 // <-- add this line
-    // gluLookAt(2,2,3,  0,0,0,  0,1,0);
-    glRotatef(45,1,1,1);
-    drawPyramid();
-    glFlush();
 }
 
-int main(int argc, char** argv) {
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+            glRotatef(angle2, 1.0f, 0.0f, 0.0f);
+        glRotatef(angle,  0.0f, 1.0f, 0.0f);
+        glScalef(zoom, zoom, zoom);
+    gluLookAt(6,6,10,  0,2,0,  0,1,0);
+    glRotatef(20, 0,1,0);
+    drawPyramid(5);                 // <-- single recursive function
+    glutSwapBuffers();
+}
+
+
+
+
+void
+mouse(int button, int state, int x, int y)
+{
+    if (state == GLUT_DOWN) {
+        mouseButton = button;
+        moving = 1;
+        startx = x;
+        starty = y;
+    }
+    if (state == GLUT_UP) {
+        mouseButton = button;
+        moving = 0;
+    }
+}
+
+void motion(int x, int y)
+{
+    if (moving) {
+        if (mouseButton == GLUT_LEFT_BUTTON)
+        {
+            angle = angle + (x - startx);
+            angle2 = angle2 + (y - starty);
+        }
+        else zoom += ((y - starty) * 0.001);
+        startx = x;
+        starty = y;
+        glutPostRedisplay();
+    }
+
+}
+
+
+int main(int argc,char** argv){
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
     glutInitWindowSize(800,800);
-    glutCreateWindow("tut 1");
+    glutCreateWindow("One-Function One-Recursion Pyramid");
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0, 0, 0, 0);
+    glutReshapeFunc(reshape);
     glutDisplayFunc(display);
+        glutMouseFunc(mouse);
+    glutMotionFunc(motion);
     glutMainLoop();
     return 0;
 }
---
+++ testt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int a = 0;
+while ((a < 35) || (a % 2) == 1) {
+    a++;
+}
+int b = a; 
+
+    printf("aaa: %d", b);
+}
